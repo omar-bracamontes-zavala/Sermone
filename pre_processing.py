35d4ac52ab952a4594ea5649cffe1157b56118ef
JulioSanchezD <julioe.sanchezd@gmail.com>
2020-04-13 20:41:30 -0500

    Terminado el pre procesamiento

--- pre_processing.py
+++ pre_processing.py
@@ -1,5 +1,42 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OrdinalEncoder
 import pandas as pd
-pd.options.mode.chained_assignment = None  # default='warn'
+
+
+class AttributesTransformator(BaseEstimator, TransformerMixin):
+    def __init__(self, days_transformer = True, boolean_mapper = True, tilde_remover = True):
+        self.days_transformer = days_transformer
+        self.boolean_mapper = boolean_mapper
+        self.tilde_remover = tilde_remover
+
+    @staticmethod
+    def date_to_days(date_series):
+        min_date = date_series.min()
+        return date_series.apply(lambda x: (x - min_date).days)
+
+    @staticmethod
+    def remove_tildes(string):
+        if string == "nan":
+            return ""
+        string = string.lower()
+        vocales = {"á": "a", "é": "e", "í": "i", "ó": "o", "ú": "u"}
+        for vocal in vocales.keys():
+            string = string.replace(vocal, vocales[vocal])
+        return string
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, df):
+        if self.days_transformer:
+            df["date"] = self.date_to_days(df["date"])
+        if self.boolean_mapper:
+            df["is_info_useful"] = df["is_info_useful"].map({"Sí": 1, "No": 0})
+        if self.tilde_remover:
+            df["missing_info"] = df["missing_info"].apply(lambda x: self.remove_tildes(x))
+            df["improvements"] = df["improvements"].apply(lambda x: self.remove_tildes(x))
+        return df
 
 
 def load_data(file_name="datasets/sugerencias.csv"):
@@ -9,38 +46,21 @@ def load_data(file_name="datasets/sugerencias.csv"):
     df = pd.read_csv(file_name, parse_dates=["Fecha"],
                      usecols=useful_cols, encoding='utf-8').drop_duplicates()
     df.columns = ["institution", "is_info_useful", "missing_info", "improvements", "date"]
+    df["missing_info"] = df["missing_info"].astype(str)
+    df["improvements"] = df["improvements"].astype(str)
     df.sort_values('date', ascending=True, inplace=True)
-    return df
+    na_filter = df["missing_info"].isna() & df["improvements"].isna()
+    return df[~na_filter]
 
 
-def date_to_days(date_series):
-    min_date = date_series.min()
-    return date_series.apply(lambda x: (x - min_date).days)
+if __name__ == "__main__":
+    df = load_data()
 
-
-def remove_tildes(string):
-    if string == "nan":
-        return ""
-    string = string.lower()
-    vocales={"á": "a", "é": "e", "í": "i", "ó": "o", "ú": "u"}
-    for vocal in vocales.keys():
-        string = string.replace(vocal, vocales[vocal])
-    return string
-
-
-df = load_data()
-na_filter = df["missing_info"].isna() & df["improvements"].isna()
-df = df[~na_filter]
-df["date"] = date_to_days(df["date"])
-df["is_info_useful"] = df["is_info_useful"].map({"Sí": 1, "No": 0})
-df["missing_info"] = df["missing_info"].astype(str)
-df["improvements"] = df["improvements"].astype(str)
-df.loc[:, "missing_info"] = df["missing_info"].apply(lambda x: remove_tildes(x))
-df.loc[:, "improvements"] = df["improvements"].apply(lambda x: remove_tildes(x))
-
-from sklearn.preprocessing import OrdinalEncoder
-
-ordinal_encoder = OrdinalEncoder()
-
-df['institution_cat'] = ordinal_encoder.fit_transform(df[['institution']])
-df.drop(["institution"], axis=1, inplace=True)
+    transf_attribs = ["is_info_useful", "missing_info", "improvements", "date"]
+    cat_attribs = ["institution"]
+    preparation_pipeline = ColumnTransformer([
+        ("transform", AttributesTransformator(), transf_attribs),
+        ("encode", OrdinalEncoder(), cat_attribs),
+    ])
+    df_prepared = preparation_pipeline.fit_transform(df)
+    print(df_prepared)
