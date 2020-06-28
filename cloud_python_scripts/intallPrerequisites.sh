85c46c1f04e61eb2a873adc7077ef3d50ed85321
Mario Fonseca <mario_fonseca@epam.com>
2020-06-28 14:18:43 -0500

    Se agrega un script para instalar anaconda y otras dependencias.

---
+++ cloud_python_scripts/intallPrerequisites.sh
@@ -0,0 +1,9 @@
+#Get prerequisites
+apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+#get anaconda installation
+curl –O https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+#Execute the installation and answer the questions
+bash ./Anaconda3-2020.02-Linux-x86_64.sh
+source ~/.bashrc~
+
+
